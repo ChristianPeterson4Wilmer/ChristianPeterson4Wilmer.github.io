df3fd5124bcf5ab8218280edf1817c562a7b3b2b
Peterson <Christian.Peterson@corp.tcc.inet>
2019-07-01 10:19:30 -0400

    Re-styling of help request page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//<span class="credit">Christian Peterson<br /><i>Technical Marketing Specialist</i><br /><b>Wilmer</b></span>
+//<span class="credit">Christian Peterson<br />i>Technical Marketing Specialist</i><br /><b>Wilmer</b></span>
 
 function load(dir) {
 	attachNavBar(dir);
@@ -28,32 +28,5 @@ function generateEmailString(type) {
 	
 	var ret = "mailto:Christian.Peterson@4wilmer.com?subject=" + "[siteupdate] " + type + " : " + username + " Additional documents" + "&body=Attach documents to this email and provide description.";
 	
-	return ret;
-}
-function socialMediaRedirect() {
-	var url = generateSocialMediaEmailString();
-	if (!url || url.length === 0) {
-		return;
-	}
-
-	window.open(url);
-}
-function generateSocialMediaEmailString() {
-	var username = document.getElementById('user-name').value;
-	var email = document.getElementById('email').value;
-	var postdate = document.getElementById('post-date').value;
-	var post = document.getElementById('post-text').value;
-
-	var arr = [username, email, postdate, post];
-
-	for (var i = 0; i < arr.length; i++) {
-		if (!arr[i] || arr[i].length === 0) {
-			alert("All fields are required!");
-			return "";
-		}
-	}
-
-	var ret = "mailto:Christian.Peterson@4wilmer.com?subject=[SocialMedia] " + postdate + "&body=[PostText] " + post;
-
 	return ret;
 }
